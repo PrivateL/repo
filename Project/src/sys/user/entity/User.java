1894bdac133fa047b2bd7cd97c050a9f3157c651
huangshichen1 <1641452570@qq.com>
2017-11-29 22:06:23 +0800

    将上一步的bug修复
    去除掉的phone属性，將set和get删除

--- Project/src/sys/user/entity/User.java
+++ Project/src/sys/user/entity/User.java
@@ -40,12 +40,6 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
 	public String getAddr() {
 		return addr;
 	}
