10fb743a47050fd9ede1883989ee520e6229eadb
huangshichen1 <1641452570@qq.com>
2017-12-10 16:22:06 +0800

    作者专栏添加、修改
    作者专栏大致做好了

--- Project/src/sys/author/AuthorColumnAction.java
+++ Project/src/sys/author/AuthorColumnAction.java
@@ -3,6 +3,10 @@ package sys.author;
 import java.util.Date;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
+
 import sys.category.Category;
 import sys.category.CategoryService;
 
@@ -17,7 +21,7 @@ public class AuthorColumnAction extends ActionSupport implements ModelDriven<Aut
 		return authorColumn;
 	}
 	
-	AuthorColumnService authorColumnService;
+	private AuthorColumnService authorColumnService;
 	public void setAuthorColumnService(AuthorColumnService authorColumnService) {
 		this.authorColumnService = authorColumnService;
 	}
@@ -25,6 +29,10 @@ public class AuthorColumnAction extends ActionSupport implements ModelDriven<Aut
 	private Integer ctgSecId;
 	public void setCtgSecId(Integer ctgSecId) {
 		this.ctgSecId = ctgSecId;
+	}
+	public void setCtgSecId(String ctgSecId){
+		System.out.println(ctgSecId);
+		this.ctgSecId = Integer.valueOf(ctgSecId);
 	}
 
 
@@ -41,15 +49,47 @@ public class AuthorColumnAction extends ActionSupport implements ModelDriven<Aut
 		return "findAll";
 	}
 	
+	// 作者专栏添加时的分类
+	public String addAuthor(){
+		List<Category> cList = categoryService.findAll();
+		// 压入值栈:
+		ActionContext.getContext().getValueStack().set("cList", cList);
+		return "addAuthor";
+	}
+	
 	// 储存作者专栏
 	public String save(){
-		System.out.println(ctgSecId);
+		HttpServletRequest request = ServletActionContext.getRequest();
+		
+		System.out.println(request.getParameter("authorPhoto"));
 		Category category = categoryService.findByid(ctgSecId);
 		authorColumn.setCategory(category);
 		authorColumn.setColumnUploadTime(new Date());
 		authorColumn.setColumnWeight(0);
 		authorColumnService.save(authorColumn);
 		return "saveSuccess";
+	}
+	
+	// 编辑作者专栏
+	public String edit(){
+		authorColumn = authorColumnService.adminFindById(authorColumn.getAcId());
+		List<Category> cList = categoryService.findAll();
+		ActionContext.getContext().getValueStack().set("cList", cList);
+		return "editSuccess";
+	}
+	
+	// 修改作者专栏
+	public String update(){
+		//System.out.println(ctgSecId);
+		authorColumn.setCategory(categoryService.findByid(ctgSecId));
+		authorColumnService.update(authorColumn);
+		return "updateSuccess";
+	}
+	
+	// 删除作者专栏
+	public String delete(){
+		authorColumnService.delete(authorColumn);
+		return "deleteSuccess";
 	}
 	
 }
